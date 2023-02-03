07f3d0769c3d23d9274e5ecfd12ab2925b881190
jigse <jayeshkjain6@gmail.com>
2023-02-03 10:20:24 +0530

    final commit

--- btc_price_converter.py
+++ btc_price_converter.py
@@ -1,6 +1,14 @@
 from telegram import Update, constants
 from telegram.ext import  CommandHandler, ApplicationBuilder,     MessageHandler, filters,ContextTypes
 import openai
+from collections import deque
+import time
+
+# Define a rate limit, in this case, 1 request per second
+RATE_LIMIT = 1
+
+# Define a queue to store the timestamps of requests
+request_queue = deque()
 
 # Define the token for your bot
 TOKEN = "5361363661:AAEJEuY1Tjalf1OfWM3tphsgFlskrFn9JDk"
@@ -15,8 +23,20 @@ async def echo_text(update: Update, context: ContextTypes.DEFAULT_TYPE):
     
     reply = update.message.text
     await context.bot.send_chat_action(chat_id=update.message.chat_id, action=constants.ChatAction.TYPING)
-   # await update.message.reply_text("Please Wait...")
-    openai.api_key = "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"
+   
+    openai.api_key = "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"
+
+    # Check if the rate limit has been exceeded
+    if len(request_queue) >= RATE_LIMIT and time.time() - request_queue[0] < 1:
+        await update.message.reply_text("You are sending messages too fast. Please wait a moment and try again.")
+        return
+
+    # Add the current timestamp to the request queue
+    request_queue.append(time.time())
+
+    # If the request queue exceeds the defined limit, remove the oldest item
+    if len(request_queue) > RATE_LIMIT:
+        request_queue.popleft()
 
 
     response = openai.Completion.create(
